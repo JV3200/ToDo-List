81fae75a1c9b78676c018efe69d4f97398445759
John Velasco <87551164+JV3200@users.noreply.github.com>
2023-07-22 20:42:47 -0700

    updated readme and code block comments

--- todos.js
+++ todos.js
@@ -1,16 +1,23 @@
 let input = prompt("what would you like to do?");
+// defining input as a prompt function with a question for the user.
 const todos = ["collect mail", "clean room", "walk dog"];
+// defining todos as an array of strings
 
 while (input !== "quit" && input !== "q") {
+  // Creates a while loop that will continue to prompt the user with the input prompt as long as the input does NOT = a string that says quit and q
   if (input === "list") {
     for (let i = 0; i < todos.length; i++) {
+      // If the users input equals list then this code will then initiate the for loop to iterate over each index of the array.
       console.log(`${i}: ${todos[i]}`);
+      // displays in the console the index number and name of the todo using string template literals.
       console.log("********");
     }
   } else if (input === "new") {
     const newTodo = prompt("Ok, what is the new todo?");
     todos.push(newTodo);
+    //if user input equals new then this block of cod will then prompt the user with a question of what the new todo item will be?
     console.log(`${newTodo} added to the list`);
+    //displays the new todo and provided string after the string template literal
   } else if (input === "delete") {
     const index = parseInt(prompt("Ok, enter an index to delete!"));
     if (!Number.isNaN(index)) {
@@ -19,8 +26,9 @@ while (input !== "quit" && input !== "q") {
     } else {
       console.log("Unknown index");
     }
+    // if the users in put equals delete it will then prompt the user to provide an index value which will be used by splice method to delete the chosen index todo item.
   }
-  input = prompt("what would you like to do?");
+  // input = prompt("what would you like to do?");
 }
 
 console.log("Goodbye!");
